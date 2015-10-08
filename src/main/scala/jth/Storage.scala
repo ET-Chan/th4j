0d39f21aabad5f7b2931b906202a0d433f18c188
Iat Chong Chan <louisivchan@gmail.com>
2015-10-08 22:12:00 +0100

    better licensing information

--- src/main/scala/jth/Storage.scala
+++ src/main/scala/jth/Storage.scala
@@ -1,29 +1,37 @@
 
-//import StorageFunc
-//
-///**
-// * Created by et on 23/09/15.
-// */
-//class Storage[T] {
-//  //actual storage holder
-////  val holderPtr:Long
-////  val func = implicitly[StorageFunc[T]]
-//  import func._
-//
-//
-//}
-package jth
 
-//do I need this ?
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Iat Chong Chan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package jth
 
 
 
 import scala.language.experimental.macros
-import scala.reflect.macros.whitebox.Context
-import scala.annotation.StaticAnnotation
-import scala.annotation.compileTimeOnly
-import jth.util.MacroHelper._
-import jth.func.{StorageCopyFunc, StorageFunc, PointerFunc}
+import jth.func._
 import jth.generate._
 import scala.NotImplementedError
 
@@ -50,15 +58,6 @@ abstract class Storage[T <: AnyVal, U <: AnyVal]{
     freePtr()
     ptr = ops.Storage_newWithData(arr, arr.length)
   }
-//
-  def ctor3(t:Storage[T, U], offset:Int, size:Int)={
-    /*Is this constructor really worthwhile?
-    * Are people normally want to take a partial view on the storage?
-    * */
-    throw new NotImplementedError
-  }
-
-  def ctor4(t:Storage[T, U]) = ctor3 (t, 0, 0)
 
   /*------------------------------------------*/
   def size() = ops.Storage_size(ptr)
@@ -93,9 +92,9 @@ abstract class Storage[T <: AnyVal, U <: AnyVal]{
         copyOps.Storage_copyLong(ptr, src.ptr)
       case src:DoubleStorage=>
         copyOps.Storage_copyDouble(ptr, src.ptr)
-      case _=>{
+      case _=>
         throw new Exception("Unknown type of storage.")
-      }
+
     }
     this
   }
--- src/main/scala/jth/func/TensorFunc.scala
+++ src/main/scala/jth/func/TensorFunc.scala
@@ -1,5 +1,3 @@
-
-
 /*
  * The MIT License (MIT)
  *
@@ -25,26 +23,12 @@
  *
  */
 
-package jth
-//this is for all type generating functions
-import scala.language.experimental.macros
-import scala.reflect.macros.whitebox.Context
-import scala.annotation.StaticAnnotation
-import scala.annotation.compileTimeOnly
+package jth.func
 
-object TypeMacro {
-  def generateAllTypeTags():List[Any]= macro generateAllTypeTagsImpl
-  def generateAllTypeTagsImpl(c:Context)():c.Expr[List[Any]]={
-    import c.universe._
-    
-    val ret = allTypes.map{case (r, (ar, prefix))=>{
-      val rtq = TypeApply(Ident(TermName("typeOf")), List(Ident(TypeName(r))))
-      val atq = TypeApply(Ident(TermName("typeOf")), List(Ident(TypeName(ar))))
-      q"""($rtq, $atq, $prefix)"""
-    }}
-    c.Expr[List[Any]](q"List(..$ret)")
-  }
+/**
+ * Created by et on 08/10/15.
+ */
+trait TensorFunc [T<:AnyVal, U<:AnyVal] {
   
-  
-  
+
 }
